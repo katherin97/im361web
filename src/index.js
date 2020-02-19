f7dbd7d7d999b714f78a12923bad3aa956a40950
katherin97 <60371103+katherin97@users.noreply.github.com>
2020-02-18 20:07:32 -0600

    Assginment 2020.02.19

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Chaos!',
-    counter: 2,
-    username: "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you"
+    userName: 'Katie'
   }
 });
 
